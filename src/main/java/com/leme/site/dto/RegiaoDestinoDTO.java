5c67a1d41a707ff459edc0f661bb33136133d095
laraacgimenes <anamenezes.dev@gmail.com>
2022-09-21 10:34:11 -0300

    Criando DTO

---
+++ src/main/java/com/leme/site/dto/RegiaoDestinoDTO.java
@@ -0,0 +1,38 @@
+package com.leme.site.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.leme.site.domain.CidadeDestino;
+
+public class RegiaoDestinoDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=150, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+	
+	public RegiaoDestinoDTO(CidadeDestino obj) {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
